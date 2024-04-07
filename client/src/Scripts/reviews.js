04ca149c5d733978647ed91eac13c6b03e6011cc
Bhuvan Sawant <102148710+bhusawant@users.noreply.github.com>
2024-04-08 00:45:00 +0530

    All folders in src uploaded

---
+++ client/src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+    {
+      "name": "Esther Howard",
+      "location": "Ahmadnagar, Maharashtra",
+      "message": "Health Connect transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "John Doe",
+      "location": "Akola, Maharashtra",
+      "message": "I found the perfect specialist for my condition through Health Connect. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Alice Smith",
+      "location": "Amravati, Maharashtra",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Bob Johnson",
+      "location": "Dhule, Maharashtra",
+      "message": "Health Connect is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Jane Brown",
+      "location": "Kalyan, Maharashtra",
+      "message": "I used to dread dental visits, but Health Connect made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+    {
+      "name": "Robert Wilson",
+      "location": "Kolhapur, Maharashtra",
+      "message": "Finally, a healthcare platform that puts patients first! The heart disease service provided comprehensive care and regular follow-ups. Thank you, Health Connect!"
+    },
+    {
+      "name": "Mary Lee",
+      "location": "Mumbai, Maharashtra",
+      "message": "I've been using Health Connect for a variety of health concerns, and each time, I received exceptional care. It's my go-to for all medical needs!"
+    },
+    {
+      "name": "David Miller",
+      "location": "Nagpur, Maharashtra",
+      "message": "I highly recommend Health Connect for online consultations. It's convenient, secure, and the doctors are top-notch. Great job, team!"
+    },
+    {
+      "name": "Sarah Johnson",
+      "location": "Nashik, Maharashtra",
+      "message": "The convenience of accessing medical notes online was fantastic. Health Connect made managing my health records hassle-free! Best Healthcare services."
+    },
+    {
+      "name": "Michael Brown",
+      "location": "Ratnagiri, Maharashtra",
+      "message": "As a busy professional, Health Connect has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!"
+    }
+];
